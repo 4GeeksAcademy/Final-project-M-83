43d1d8895a2a0b4b81d359eb1722e587a56c1344
Britney Escoffery <britneybescoffery@gmail.com>
2025-10-03 23:15:53 +0000

    added routes for different pages

---
+++ src/front/pages/Islanders.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+
+const Islanders = () => {
+    return(
+        <div>
+            All Islanders Page
+        </div>
+    )
+}
+
+export default Islanders;
