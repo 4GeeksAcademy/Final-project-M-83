db4f80aa547756bc8d91de049ef50b99583a386c
Kaleb Williams <kalebklw@gmail.com>
2025-10-04 00:52:53 +0000

    progress on user model table

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Column, Integer, Table, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+favorite_islanders = Table (
+    "favorite_islanders",
+    db.Model.metadata,
+    Column ("user_id", Integer, ForeignKey("user.id"), primary_key = True),
+    Column ("islander_id", Integer, ForeignKey("islander.id"), primary_key = True)
+)
+
+favorite_bombshells = Table(
+    "favorite_bombshells",
+    db.Model.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key = True),
+    Column("bombshell_id", Integer, ForeignKey("bombshell.id"), primary_key = True)
+)
+
+
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id = Column(Integer, primary_key = True) 
+    email = Column(String(70), unique = True, nullable = False)
+    favorite_islanders = relationship("Islanders", secondary = favorite_islanders)
+    favorite_bombshells = relationship("Bombshells", secondary = favorite_bombshells)
+    password = Column(String(70), unique = False, nullable = False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "favorite_islanders": [item.serialize() for item in self.favorite_islanders],
+            "favorite_bombshells": [item.serialize() for item in self.favorite_bombshells]
             # do not serialize the password, its a security breach
         }
