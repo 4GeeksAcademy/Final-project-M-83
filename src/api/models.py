ed6178c9687833a16a4e63f6708127529b7dd0bc
Kaleb Williams <kalebklw@gmail.com>
2025-10-06 23:37:28 +0000

    Added in the code for the bomshell model table. Made 'status' a boolean to help differentiate the bombshells from the islanders

--- src/api/models.py
+++ src/api/models.py
@@ -3,11 +3,11 @@ from sqlalchemy import String, Boolean, Column, Integer, Table, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
-favorite_islanders = Table (
+favorite_islanders = Table(
     "favorite_islanders",
     db.Model.metadata,
-    Column ("user_id", Integer, ForeignKey("user.id"), primary_key = True),
-    Column ("islander_id", Integer, ForeignKey("islander.id"), primary_key = True)
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("islander_id", Integer, ForeignKey("islander.id"), primary_key=True)
 )
 
 # favorite_bombshells = Table(
@@ -18,21 +18,19 @@ favorite_islanders = Table (
 # )
 
 
-
 class User(db.Model):
     __tablename__ = "user"
-    id = Column(Integer, primary_key = True) 
-    email = Column(String(70), unique = True, nullable = False)
-    favorite_islanders = relationship("Islander", secondary = favorite_islanders)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(70), unique=True, nullable=False)
+    username = Column(String(70), unique=True, nullable=False)
+    phonenumber = Column(Integer, unique=True, nullable=True)
+    profile_image = Column(String(260), nullable=True)
+    favorite_islanders = relationship("Islander", secondary=favorite_islanders)
     # favorite_bombshells = relationship("Bombshells", secondary = favorite_bombshells)
-    username = Column(String(70), unique = True, nullable = False)
-    phonenumber = Column(Integer, unique = True, nullable = True)
-    profile_image = Column(String(260), nullable = True)
-    password = Column(String(70), unique = False, nullable = False)
-    
-    def serialize(self):
+    password = Column(String(70), unique=False, nullable=False)
 
-        return{
+    def serialize(self):
+        return {
             "id": self.id,
             "username": self.username,
             "phonenumber": self.phonenumber,
@@ -50,14 +48,34 @@ class Islander (db.Model):
     age = Column(Integer, nullable=True)
     occupation = Column(String(100), nullable=True)
     hometown = Column(String(100), nullable=True)
-    status = Column(String(20), default="active")  
-    
+    status = Column(String(20), default="active")
+
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
             "age": self.age,
-            "occupation" : self.occupation,
+            "occupation": self.occupation,
             "hometown": self.hometown,
+            "status": self.status
+        }
+
+
+class Bombshell (db.Model):
+    __tablename__ = "bombshells"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    age = Column(Integer, nullable=False)
+    hometown = Column(String(100), nullable=False)
+    occupation = Column(String(100), nullable=False)
+    status = Column(Boolean, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "hometown": self.hometown,
+            "occupation": self.occupation,
             "status": self.status
         }
