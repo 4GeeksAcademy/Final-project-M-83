714b15af020dad99624c29ace269dba4552c041f
Kaleb Williams <kalebklw@gmail.com>
2025-10-07 00:04:21 +0000

    Deleted the bombshell model branch, and switched the 'status' variable to 'bombshell' (setting it as a boolean with default equal to false) within the Islander Model table to.

--- src/api/models.py
+++ src/api/models.py
@@ -10,13 +10,6 @@ favorite_islanders = Table(
     Column("islander_id", Integer, ForeignKey("islander.id"), primary_key=True)
 )
 
-# favorite_bombshells = Table(
-#     "favorite_bombshells",
-#     db.Model.metadata,
-#     Column("user_id", Integer, ForeignKey("user.id"), primary_key = True),
-#     Column("bombshell_id", Integer, ForeignKey("bombshell.id"), primary_key = True)
-# )
-
 
 class User(db.Model):
     __tablename__ = "user"
@@ -26,7 +19,6 @@ class User(db.Model):
     phonenumber = Column(Integer, unique=True, nullable=True)
     profile_image = Column(String(260), nullable=True)
     favorite_islanders = relationship("Islander", secondary=favorite_islanders)
-    # favorite_bombshells = relationship("Bombshells", secondary = favorite_bombshells)
     password = Column(String(70), unique=False, nullable=False)
 
     def serialize(self):
@@ -37,7 +29,6 @@ class User(db.Model):
             "profile_image": self.profile_image,
             "email": self.email,
             "favorite_islanders": [item.serialize() for item in self.favorite_islanders],
-            # "favorite_bombshells": [item.serialize() for item in self.favorite_bombshells]
         }
 
 
@@ -48,7 +39,7 @@ class Islander (db.Model):
     age = Column(Integer, nullable=True)
     occupation = Column(String(100), nullable=True)
     hometown = Column(String(100), nullable=True)
-    status = Column(String(20), default="active")
+    bombshell = Column((Boolean), default=False)
 
     def serialize(self):
         return {
@@ -57,25 +48,5 @@ class Islander (db.Model):
             "age": self.age,
             "occupation": self.occupation,
             "hometown": self.hometown,
-            "status": self.status
-        }
-
-
-class Bombshell (db.Model):
-    __tablename__ = "bombshells"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    age = Column(Integer, nullable=False)
-    hometown = Column(String(100), nullable=False)
-    occupation = Column(String(100), nullable=False)
-    status = Column(Boolean, nullable=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "age": self.age,
-            "hometown": self.hometown,
-            "occupation": self.occupation,
-            "status": self.status
+            "bombshell": self.bombshell
         }
