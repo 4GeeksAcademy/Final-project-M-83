60a7f9db9dfc5cc10b1d8c5fed7b326254c5a478
Britney Escoffery <britneybescoffery@gmail.com>
2025-10-29 23:28:45 +0000

    rendered female islander photos to front end

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -15,8 +15,30 @@ export const Home = () => {
 		const allseasonsgirls = girls.bachelor //filtering all girls from the bachelor seasons
 		const season23datagirls = allseasonsgirls["23"] //filtering all contestant data from S23
 		const fcontestantslist = season23datagirls.contestants //actual list of female contestants in S23
-		fcontestantslist.length = 15; //declares length of female islanders we are choosing, 15 
-		fcontestantslist[0].photo_url = "https://images.unsplash.com/photo-1699061930674-1be64fe86fc3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=683"
+		fcontestantslist.length = 15; //limit female islander list to 15
+		// set up array of urls for female photos
+		const fphotoUrls = [
+			"https://images.unsplash.com/photo-1699061930674-1be64fe86fc3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=683",
+			"https://images.unsplash.com/photo-1742891734968-4f1822442265?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=928",
+			"https://images.unsplash.com/photo-1713433366641-4138ec16c909?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=930",
+			"https://images.unsplash.com/photo-1729218621053-dd15827d5691?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1563049077-3dfdc7018225?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=772",
+			"https://images.unsplash.com/photo-1757627569807-248bcb0613ba?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1632883515179-4be5d49a0a0d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1520065949650-380765513210?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=928",
+			"https://images.unsplash.com/flagged/photo-1570700005880-4ecdb8595d4a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1525943837837-af668e09139d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1635350296718-fdee22f7e079?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1632984813506-20e0a94ea806?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=928",
+			"https://images.unsplash.com/photo-1610666652096-07e42724bfeb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1565187566069-7e7e44267790?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774",
+			"https://images.unsplash.com/photo-1695990189962-44daba7c8067?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774"
+		];
+		//assign photo urls to islanders dynamically
+		fcontestantslist.forEach((contestant, index) => {
+			contestant.photo_url = fphotoUrls[index] || contestant.photo_url; 
+		});
+		// fcontestantslist[0].photo_url = "https://images.unsplash.com/photo-1699061930674-1be64fe86fc3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=683"
 		for (let i = 0; i <= 4; i++) {
 			const selectcontestant = fcontestantslist[i] //	 
 			selectcontestant.bombshell = true //adding bombshell as an object, we need to do this to 5 random females to fcontestants
@@ -37,6 +59,7 @@ export const Home = () => {
 		console.log("list of male contestants!!!", mcontestantslist)
 	}
 	useEffect(() => {
+		actions.getAllIslanders(store, setGirls)
 		fetch("/data.json")
 			.then((resp) => resp.json())
 			.then((data) => {
@@ -45,12 +68,25 @@ export const Home = () => {
 				getIslanderBoys(data)
 			})
 	}, [])
-	console.log("Ggirls!!!!", girls)
-	const girlImageUrl = girls.length > 0 ? girls[0].photo_url : ""
+	console.log("girls!!!!", girls)
 	return (
 		<div>
-			<img src={girlImageUrl}
-				width="200" />
+			<h2>Meet the Girls</h2>
+			<div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+				{girls.map((girl, index) => (
+					<div key={index} style={{ textAlign: "center" }}>
+						<img
+							src={girl.photo_url}
+							alt={girl.name || `Contestant ${index + 1}`}
+							width="150"
+							height="150"
+							style={{ borderRadius: "10px", objectFit: "cover" }}
+						/>
+						<p>{girl.name || "Unknown"}</p>
+						{girl.bombshell && <p style={{ color: "red" }}>💣 Bombshell</p>}
+					</div>
+				))}
+			</div>
 			<Leaderboard />
 		</div>
 	);
